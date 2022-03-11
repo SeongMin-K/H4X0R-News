92ea62b854a5df976d5ff850a04a0b9a1b2cbc6e
SeongMin-K <eo1tks3@naver.com>
2022-03-11 17:20:46 +0900

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by SeongMinK on 2022/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
